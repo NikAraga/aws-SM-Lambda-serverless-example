d9813220e68ea74b47e6e51fefaa091f50626eaf
nikaraga <nikhil@flux7.com>
2019-04-14 11:51:50 +0530

    Created Serverless for AwS

---
+++ lambda_function_1.py
@@ -0,0 +1,6 @@
+import os
+
+def lambda_handler(event, context):
+    print('Test Function-1')
+    return True
+    
---
+++ lambda_function_2.py
@@ -0,0 +1,6 @@
+import os
+
+def lambda_handler(event, context):
+    print('Test Function-2')
+    return True
+    
